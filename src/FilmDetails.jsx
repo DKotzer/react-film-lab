6a9ec1f5b9eebf83847c9e21c6d59f0ca8288bbe
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-16 23:24:41 -0400

    about the same

--- src/FilmDetails.jsx
+++ src/FilmDetails.jsx
@@ -3,19 +3,6 @@ import React from "react";
 export default function FilmDetails(props) {
   console.log("props.film", props.film);
   console.log("faves list", props.fave_status);
-  const backdropUrl = `https://image.tmdb.org/t/p/w1280${props.film.backdrop_path}`;
-  const posterUrl = `https://image.tmdb.org/t/p/w780${props.film.poster_path}`;
-  let details = (
-    <div>
-      <i className='material-icons'>subscriptions</i>{" "}
-      <span>No film selected</span>
-    </div>
-  );
-
-  let fave_icon = "";
-  if (props.fave_status === true) {
-    fave_icon = "♡";
-  }
 
   let submit_review = (e) => {
     e.preventDefault();
@@ -25,15 +12,21 @@ export default function FilmDetails(props) {
     console.log(formDataObj.reviewInput);
     props.film.review = formDataObj.reviewInput;
     console.log("review test", props.film.review);
-    if (props.film.review) {
-      review_area = (
-        <div className='reviewarea'>
-          <p>{props.film.review}</p>
-        </div>
-      );
-    }
+
+    setReview(
+      <div className='reviewarea'>
+        <h5>Review:</h5>
+        <p>{props.film.review}</p>
+      </div>
+    );
+    // review_area = (
+    //   <div className='reviewarea'>
+    //     <p>{props.film.review}</p>
+    //   </div>
+    // );
   };
-  let review_area = (
+
+  const [review, setReview] = React.useState(
     <div className='reviewarea'>
       <h5>Enter Review</h5>
       <form onSubmit={submit_review}>
@@ -49,13 +42,44 @@ export default function FilmDetails(props) {
       </form>
     </div>
   );
-  if (props.film.review) {
-    review_area = (
-      <div className='reviewarea'>
-        <p>{props.film.review}</p>
-      </div>
-    );
+
+  const backdropUrl = `https://image.tmdb.org/t/p/w1280${props.film.backdrop_path}`;
+  const posterUrl = `https://image.tmdb.org/t/p/w780${props.film.poster_path}`;
+  let details = (
+    <div>
+      <i className='material-icons'>subscriptions</i>{" "}
+      <span>No film selected</span>
+    </div>
+  );
+
+  let fave_icon = "";
+  if (props.fave_status === true) {
+    fave_icon = "♡";
   }
+
+  // let review_area = (
+  //   <div className='reviewarea'>
+  //     <h5>Enter Review</h5>
+  //     <form onSubmit={submit_review}>
+  //       <textarea
+  //         name='reviewInput'
+  //         id='review-input'
+  //         cols='30'
+  //         rows='10'
+  //       ></textarea>
+  //       <button variant='primary' type='submit'>
+  //         Submit Review
+  //       </button>
+  //     </form>
+  //   </div>
+  // );
+  // if (props.film.review) {
+  //   review_area = (
+  //     <div className='reviewarea'>
+  //       <p>{props.film.review}</p>
+  //     </div>
+  //   );
+  // }
 
   if (props.film.title) {
     details = (
@@ -79,7 +103,7 @@ export default function FilmDetails(props) {
             {props.film.overview}
           </div>
         </div>
-        {review_area}
+        {review}
       </div>
     );
   }
